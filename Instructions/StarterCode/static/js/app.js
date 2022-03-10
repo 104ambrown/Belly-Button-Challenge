2e4471b1e30f24c70715d17b48438beaaa076107
Zas Mama <104ambrown@gmail.com>
2022-03-09 23:31:53 -0500

    added code to app.js for building the wash frequency gauge

--- Instructions/StarterCode/static/js/app.js
+++ Instructions/StarterCode/static/js/app.js
@@ -1,6 +1,6 @@
 // Plotly Belly Button Biodiversity Homework
 // Writing a function to pull the data in
-function getData(bbInfo){
+function getData(bbData){
     d3.json("samples.json").then((data)=> {
         var metadata = data.metadata;
         //console.log(metadata);
@@ -14,6 +14,18 @@ function getData(bbInfo){
         Object.entries(resultsArray).forEach((key, value) => {
             survey.append("h6").text(`${key}: ${value}`);
         });
+
+// Washing Frequency gauge
+function wfGauge(bbData) {
+    d3.json("samples.json").then((data)=> {
+        var metadata = data.metadata;
+        var gaugeArray = metadata.filter(info => info.id.toString() == bbInfo);
+        var gaugeResults = gaugeArray[0]
+        var survey = d3.select("#bbInfo-metadata");
+        survey.html("");
+        Object.entries(gaugeArray).forEach((key, value) => {
+            survey.append("h6").text(`${key}: ${value}`);
+        });
+}
     })
 }
-
