c5f50cc891c92498b9507c2aa984cc09ce1599d6
Zas Mama <104ambrown@gmail.com>
2022-03-09 23:49:57 -0500

    Filtering by id for was h frequency

--- Instructions/StarterCode/static/js/app.js
+++ Instructions/StarterCode/static/js/app.js
@@ -7,18 +7,34 @@ function getData(bbData){
 
         
         // Filtering belly button data by sample_values and ID
-        var resultsArray = metadata.filter(info => info.id.toString() == bbInfo);
+        var resultsArray = metadata.filter(info => info.id.toString() == bbData);
         var results = resultsArray[0]
-        var survey = d3.select("#bbInfo-metadata");
-        survey.html("");
+        var panel = d3.select("#bbInfo-metadata");
+        // Clearing existing metadata
+        panel.html("");
+        // Adding eac hkey value pair to panel
         Object.entries(resultsArray).forEach((key, value) => {
-            survey.append("h6").text(`${key}: ${value}`);
+            panel.append("p").text(`${key}: ${value}`);
         });
+    });
+};
+
+// basic function of pulling data in for the plots
+function plotThis(bbData) {
+    d3.json("samples.json").then((data)=> {
+        //console.log(data);
+
+        // Using ID to filter wash frequency
+        var washFrequency = data.metadata.filter(wf.id.toString() === bbData)[0];
+        washFrequency = washFrequency.washFrequency;
+        //console.log("Washing frequency: " + wasFrequency);
+}
+
 
 // Washing Frequency gauge
-function wfGauge(bbData) {
+function washFreqGauge(bbData) {
     d3.json("samples.json").then((data)=> {
-        var metadata = data.metadata;
+        var objects = data.metadata;
         var gaugeArray = metadata.filter(info => info.id.toString() == bbInfo);
         var gaugeResults = gaugeArray[0]
         var survey = d3.select("#bbInfo-metadata");
@@ -27,5 +43,5 @@ function wfGauge(bbData) {
             survey.append("h6").text(`${key}: ${value}`);
         });
 }
-    })
+    });
 }
