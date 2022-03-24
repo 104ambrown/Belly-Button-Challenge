bd758ab82360ed3956b2b8aa68e39970827545fa
Zas Mama <104ambrown@gmail.com>
2022-03-24 13:10:20 -0400

    working on the gauge chart in app.js

--- Instructions/StarterCode/static/js/app.js
+++ Instructions/StarterCode/static/js/app.js
@@ -1,13 +1,13 @@
 // Plotly Belly Button Biodiversity Homework
 // Writing a skeleton function to pull the data in
-function getData(bbData){
+function getData(sample){
     d3.json("samples.json").then((data)=> {
         var metadata = data.metadata;
         //console.log(metadata);
         // Filtering belly button data by sample_values and ID
-        var resultsArray = metadata.filter(info => info.id == bbData);
+        var resultsArray = metadata.filter(info => info.id == sample);
         var result = resultsArray[0]
-        var panel = d3.select("#bbInfo-metadata");
+        var panel = d3.select("#sample-metadata");
         // Clearing existing metadata
         panel.html("");
         // Adding each key value pair to panel
@@ -17,11 +17,11 @@ function getData(bbData){
     });
 };
 
-// Writing a function to build the charts using d3.json to retrieve the somple data
-function buildCharts(bbData) {
+// Writing a function to build the  bar and bubble charts using d3.json to retrieve the somple data
+function buildCharts(sample) {
 d3.json("samples.json").then((data) => {
     var samples= data.samples;
-    var resultsArray = metadata.filter(info => info.id == bbData);
+    var resultsArray = metadata.filter(info => info.id == sample);
     var result = resultsArray[0]
     var ids = result.otu_labels;
     var values = result.sample_values;
@@ -62,19 +62,72 @@ var bubbleData = [
         }
     }
 ];
-// building a washing frequency gauge //
+Plotly.newPlot("bubble", bubbleData, bubbleLayout);
+
+// Function to build the gauge chart
+// used https://htmlcolorcodes.com/color-picker/        
+var gaugeColorPallette = ["#fffff", "#B0FFB5", "#7Aff82","#4FEE58", "#4FEEA8", "#4FE5EE", "#4F95EE", "#584FEE", "#A84FEE"
 
 
+function buildAGauge(data {
+    console.log("buildAGauge", data);
+    if(data.wfreq === null){
+        data.wfreq = 0;
+    }
+    let degree = parseInt(data.wfreq) * (180/10);
+    // Calculating meter points
+    let degrees = 180 - degree;
+    let radius = .5;
+    let radians = degrees * Math.PI / 180;
+    let x = radius * Math.cos(radians);
+    let y = radius * Math.sin(radians);
 
+    let mainPath = 'M -.0 -0.025 L .0 0.025 L ',
+        xPath = String(x),
+        space = ' ',
+        yPAth = String(y),
+        pathEnd = ' Z';
+    let path = mainPath.concat(xPath, space, yPath, pathEnd);
+    let trace = [{ type: "scatter",
+        x:[0], y:[0],
+        marker: {size: 50, color: "923DE9"},
+        showlegend: false,
+        name: "Wash Frequency",
+        text: data.wfreq,
+        hoverinfo: "text+name"},
+        {values: [1, 1, 1, 1, 1, 1, 1, 1, 1, 9],
+        rotation: 90,
+        text:["8-9", "7-8", "6-7", "5-6", "4-5", "3-4", "2-3", "1-2", "0-1", ""],
+        textInfo: "text",
+        textPosition: "inside",
+        textFont:{
+            size: 14,},
+        marker: {colors:[...gaugeColorPallette]},
+        labels:["8-9", "7-8", "6-7", "5-6", "4-5", "3-4", "2-3", "1-2", "0-1", ""],
+        hoverInfo: "text",
+        hole: .5,
+        type: "pie",
+        showlegend: false
+}];
 
-
-
-
-
-// basic function of pulling data in for the plots
-function plotThis(bbData) {
+// basic function of pulling data in for the dropdown menu
+function initialize() {
+var selection = d3.select("#selDataset");
     d3.json("samples.json").then((data)=> {
+        var sampleIDs = data.names;
+        sampleIDs.forEach((sample) => {
+            selection
+                .append("option")
+                .text(sample)
+                .property("value", sample);
+        });
+        const firstSample = sampleIDs[0];
+        getData(firstSample);
+        buildCharts(firstSample);
+        build
+
         //console.log(data);
+
 
         // Using ID to filter wash frequency
         var washFrequency = data.metadata.filter(wfreq => wfreq.id.toString() === bbData)[0];
